fcc765e3361465c9ea0baf3c726030562912e467
rowgregory <rowgregory@gmail.com>
2025-02-22 12:59:36 -0500

    chore(cors):Add headers to fetch testimonials public api

--- app/api/testimonial/fetch-testimonials-public/route.ts
+++ app/api/testimonial/fetch-testimonials-public/route.ts
@@ -1,9 +1,21 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import prisma from '@/prisma/client'
 import createLog from '@/app/utils/api/createLog'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
+    // Set CORS headers to allow requests from your frontend
+    const headers = new Headers()
+    headers.set('Access-Control-Allow-Origin', 'https://www.saltwaterbookkeeping.com') // Frontend URL
+    headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    headers.set('Access-Control-Allow-Credentials', 'true')
+
+    // Handle preflight OPTIONS request
+    if (req.method === 'OPTIONS') {
+      return new NextResponse(null, { status: 200, headers })
+    }
+
     const testimonials = await prisma.testimonial.findMany({
       orderBy: { createdAt: 'desc' },
       where: { isVisible: true }
